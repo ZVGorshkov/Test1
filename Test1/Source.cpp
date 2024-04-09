426b4d580a1af32a290479a606d30ff35ae22e3b
Zakhar <zakhar200915@yandex.ru>
2024-04-09 19:59:36 +0400

    add rerandom array

--- Test1/Source.cpp
+++ Test1/Source.cpp
@@ -22,53 +22,56 @@ void main()
 	for (int i = 0; i < size; i++)
 		arr[i] = rand() % 10;
 
-		while (true)
-		{
-			system("cls");
-			cout << "1. Вывести массив" << endl;
-			
-			cout << "2. Вывести массив индексами" << endl;
-			
-			cout << "3. Отсортировать по возрастанию" << endl;
-			cout << "4. Отсортировать по убыванию" << endl;
-			cout << "0. Выход" << endl;
+	while (true)
+	{
+		system("cls");
+		cout << "1. Вывести массив" << endl;
+		cout << "2. Вывести массив индексами" << endl;
+		cout << "3. Отсортировать по возрастанию" << endl;
+		cout << "4. Отсортировать по убыванию" << endl;
+		cout << "5. Перезаполнить массив" << endl;
+		cout << "0. Выход" << endl;
 
-			int input;
-			cin >> input;
+		int input;
+		cin >> input;
 
-			if (input == 0)
-				break;
-
-			switch (input)
-			{
-			case 1:
-				for (int i = 0; i < size; i++)
-				{
-					cout << arr[i] << " ";
-					cout << endl;
-				}
-				break;
-			case 2:
-				for (int i = 0; i < size; i++)
-				{
-					cout << "[" << i << "]" << arr[i] << endl;
-				}
-				system("pause");
-				break;
-			case 3:
-				sort(arr, arr + size);
-
-			
+		if (input == 0)
 			break;
-			case 4:
-				sort(arr, arr + size, [](int a, int b) {return a > b;});
 
-		
-		break;
+		switch (input)
+		{
+		case 1:
+			for (int i = 0; i < size; i++)
+			{
+				cout << arr[i] << " ";
+				cout << endl;
+			}
+			break;
 
+		case 2:
+			for (int i = 0; i < size; i++)
+			{
+				cout << "[" << i << "]" << arr[i] << endl;
+			}
+			system("pause");
+			break;
+
+		case 3:
+			sort(arr, arr + size);
+			break;
+
+		case 4:
+			sort(arr, arr + size, [](int a, int b) {return a > b; });
+			break;
+
+		case 5:
+			for (int i = 0; i < size; i++)
+				arr[i] = rand() % 10;
+			break;
 
 		}
 
+	}
 
 	system("pause");
 }
